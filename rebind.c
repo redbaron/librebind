6bb61dce9b9bb802e2bd2770efc78b0e05517a9b
Maxim Ivanov <ivanov.maxim@gmail.com>
2015-03-26 19:19:55 +0000

    Cleaner code

--- rebind.c
+++ rebind.c
@@ -14,30 +14,31 @@
 
 static int(*_bind)(int sockfd, const struct sockaddr *addr, socklen_t addrlen) = NULL;
 
+static int parse_env(in_port_t *from_port, in_port_t *to_port) {
+	char *rebind_port_env = getenv(REBIND_PORT_ENV), *sep = NULL, *to_port_c = NULL;
+
+	if (NULL == rebind_port_env) return -1; //no env var
+	if (NULL == (sep = strchr(rebind_port_env,':'))) return -1; // no ':' separator
+	*sep = 0;
+	to_port_c = sep + 1;
+
+	*from_port = (in_port_t)strtol(rebind_port_env, &sep, 10);
+	if (*sep != '\0') return -1; //there is symbol ':', but string before it is not a number
+	*to_port = (in_port_t)strtol(to_port_c, &sep, 10);
+	if (*sep != '\0') return -1; //there is symbol ':', but string after it is not a number
+        return 0;
+}
+
 int bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen) {
 	if (NULL == _bind) {
 		_bind = (int (*)(int sockfd, const struct sockaddr *addr, socklen_t addrlen)) dlsym(RTLD_NEXT, "bind");
 	}
         struct sockaddr_in rebindaddr;
+	in_port_t from_port, to_port;
 
         // some different socket type or other incompatibility
         if (addr->sa_family != AF_INET || addrlen != sizeof(rebindaddr)) goto fallback;
-
-	char *rebind_port_env = getenv(REBIND_PORT_ENV);
-        char *sep = NULL;
-        char *to_port_c = NULL;
-      
-	if (NULL == rebind_port_env) goto fallback; //no env var
-	if (NULL == (sep = strchr(rebind_port_env,':'))) goto fallback; // no ':' separator
-        *sep = 0;
-        to_port_c = sep + 1;
-
-	in_port_t from_port = (in_port_t)strtol(rebind_port_env, &sep, 10);
-        if (*sep != '\0') goto fallback; //there is symbol ':', but string before it is not a number
-	in_port_t to_port = (in_port_t)strtol(to_port_c, &sep, 10);
-        if (*sep != '\0') goto fallback; //there is symbol ':', but string after it is not a number
-
-
+	if (parse_env(&from_port, &to_port)) goto fallback;
         if (((struct sockaddr_in*)addr)->sin_port != htons(from_port)) goto fallback; //request port doesnt match REBIND_PORT_FROM
 
 	memcpy(&rebindaddr, addr, addrlen);
